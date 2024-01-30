95b2d2996ce1ba34cc31078ede6c602a0ae2072f
Kajol Sutra dhar <kajol.777@gmail.com>
2024-01-30 10:29:30 +0200

    While loop Task 3

---
+++ week_3/exercises/while_loops/task_03.php
@@ -0,0 +1,12 @@
+<?php
+// open the file - if it fails, terminate the program
+$myfile = fopen("datafile.txt", "r") or die("Unable to open file!");
+
+// "feof" means "file: end-of-file", so loop while we haven't reached the end of file yet
+while(!feof($myfile)) {
+    // let's get one line and put it into the variable $line
+    $line = fgets($myfile);
+    // YOUR TURN! Print that line to console.
+    print $line;
+}
+fclose($myfile);
